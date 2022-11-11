ae66c7500c530acad5687424e5ae347339e3bda1
Gordan Milotic <gmilotic@unipu.hr>
2022-11-11 22:20:35 +0100

    ne radi javascript za loginVlasnik.html

--- frontend/loginVlasnik.js
+++ frontend/loginVlasnik.js
@@ -1,31 +1,17 @@
-const { json } = require("express/lib/response");
+const ime = document.getElementById('ime')
+const prezime = document.getElementById('prezime')
+const sifraObjekta = document.getElementById('sifraObjekta')
+const lozinka = docuemnt.getElementById('lozinka')
+const form = document.getElementById('form')
+const errorElement = docuemnt.getElementById('error')
 
-const form = document.getElementById('form');
-const ime = document.getElementById('ime');
-const prezime = document.getElementById('prezime');
-const sifraObjekta = document.getElementById('sifraObjekta');
-const lozinka = document.getElementById('lozinka');
-
-form.addEventListener('submit', (e) =>{
-    e.preventDefault();
-    checkInput();
-} );
-
-const checkInput = () => {
-    const imeValue = ime.value.trim()
-    const prezimeValue = prezime.value.trim()
-    const sifraObjektaValue = sifraObjekta.value.trim()
-    const lozinkaValue = lozinka.value.trim()
-
-    if(ime === ''){
-        alert("Ovo polje ne smije biti prazno!")
+form.addEventListener('submit', (e) => {
+    let messages = []
+    if (ime.value === '' || ime.value == null) {
+        messages.push("Ime je obavezno")
     }
-}
-
-let setErrorFor = (input, message) => {
-    const text_input = input.parentElement;
-    const input = formControl.querySelector('input');
-
-    input.innerText = message;
-    formControl.className = 'text_input';
-}
+    if (messages.length > 0) {
+        e.preventDefault()
+        errorElement.innerText = messages.join(', ')
+    }
+})
--- frontend/loginZaspolenik.js
+++
@@ -1,6 +0,0 @@
-const button = document.querySelector('.zaposlenik');
-
-button.addEventListener('click', () => {
-  button.classList.toggle('active');
-  button.style.width='120px';
-});
